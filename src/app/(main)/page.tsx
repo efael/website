6eb6e2f48198d770731bc1904c282c1044e6cc18
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-07-03 22:29:27 +0500

    chore: remove qr and reviews

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,6 @@ import { Faqs } from '@/components/Faqs'
 import { Hero } from '@/components/Hero'
 import { Pricing } from '@/components/Pricing'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
-import { Reviews } from '@/components/Reviews'
 import { SecondaryFeatures } from '@/components/SecondaryFeatures'
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
       <PrimaryFeatures />
       <SecondaryFeatures />
       <CallToAction />
-      {/* <Reviews /> */}
       <Faqs />
     </>
   )
